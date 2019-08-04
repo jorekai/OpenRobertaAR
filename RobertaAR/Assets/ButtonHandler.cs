571e68d464e88b1b8b47781645d73d6373e3561f
SeferUl <ulutas@stud.fra-uas.de>
2019-08-04 21:08:23 +0200

    Button Tests on UI

---
+++ RobertaAR/Assets/ButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void setText (string text)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+
+    }
+
+
+
+}
